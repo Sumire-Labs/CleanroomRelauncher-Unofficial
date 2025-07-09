96be1be91f5df09dd228d4719cb0e99e39462ad8
pepeyukke <amukat1201.ta@gmail.com>
2025-07-09 19:24:14 +0900

    fix

--- src/main/java/com/cleanroommc/relauncher/gui/RelauncherGUI.java
+++ src/main/java/com/cleanroommc/relauncher/gui/RelauncherGUI.java
@@ -208,7 +208,7 @@ public class RelauncherGUI extends JDialog {
         GraphicsEnvironment env = GraphicsEnvironment.getLocalGraphicsEnvironment();
         GraphicsDevice screen = env.getDefaultScreenDevice();
         Rectangle rect = screen.getDefaultConfiguration().getBounds();
-        int width = rect.width / 3;
+        int width = rect.width * 2 / 9;
         int height = (int) (width / 1.25f);
         int x = (rect.width - width) / 2;
         int y = (rect.height - height) / 2;
